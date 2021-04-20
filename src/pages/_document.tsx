c46d6237ac7edc1c05a8166c64118889124686d4
Robson Barbosa Souza <powerrob92@gmail.com>
2021-04-20 10:38:39 -0300

    chore: make global styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  render() {
+  render(): JSX.Element {
     return (
       <Html lang="pt-BR">
         <Head>
           <link href="https://fonts.gstatic.com" rel="preconnect" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@600&display=swap" rel="stylesheet" />           <link rel="shortcut icon" href="/favicon.ico" type="image/ico" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap"
+            rel="stylesheet"
+          />{' '}
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
