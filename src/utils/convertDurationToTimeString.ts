b31c115d98e3878389b532ed1d6db6874aac132e
Robson Barbosa Souza <powerrob92@gmail.com>
2021-04-22 23:02:05 -0300

    fix: change time to remove on hours is zero

--- src/utils/convertDurationToTimeString.ts
+++ src/utils/convertDurationToTimeString.ts
@@ -3,9 +3,17 @@ export function convertDurationToTimeString(duration: number): string {
   const minutes = Math.floor((duration % 3600) / 60);
   const seconds = Math.floor(duration % 60);
 
-  const timeString = [hours, minutes, seconds]
-    .map(unit => String(unit).padStart(2, '0'))
-    .join(':');
+  let timeString: string;
+
+  if (hours === 0) {
+    timeString = [minutes, seconds]
+      .map(unit => String(unit).padStart(2, '0'))
+      .join(':');
+  } else {
+    timeString = [hours, minutes, seconds]
+      .map(unit => String(unit).padStart(2, '0'))
+      .join(':');
+  }
 
   return timeString;
 }
